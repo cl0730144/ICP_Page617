e972c98ba830029cbc27623e951e51becf0ad7cd
Chris Liddell <59884098+cl0730144@users.noreply.github.com>
2020-04-18 12:06:52 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,86 @@
+/*Individual Case Project
+  Author: Chris Liddell
+  Date:   April 18, 2020*/
+
+"use strict";
+
+var list = [];
+
+function generateList() {
+	var listItems = document.getElementsByTagName("li");
+	for (var i = listItems.length - 1; i >= 0; i--) {
+		document.getElementsByTagName("ol")[0].removeChild(listItems[i]);
+	}
+	for (var i = 0; i < list.length; i++) {
+		var newItem = "<span class='first'>first</span>" + "<span class='last'>last</span>" + list[i];
+		var newListItem = document.createElement("li");
+		newListItem.innerHTML = newItem;
+		document.getElementsByTagName("ol")[0].appendChild(newListItem);
+		var firstButtons = document.querySelectorAll(".first");
+		var lastFirstButton = firstButtons[firstButtons.length - 1];
+		var lastButtons = document.querySelectorAll(".last");
+		var lastLastButton = lastButtons[lastButtons.length - 1];
+		if (lastFirstButton.addEventListener) {
+			lastFirstButton.addEventListener("click", moveToTop, false);
+			lastLastButton.addEventListener("click", moveToBottom, false);
+		} else if (lastFirstButton.attachEvent) {
+			lastFirstButton.attachEvent("onclick", moveToTop);
+			lastLastButton.attachEvent("onclick", moveToBottom);
+		}
+	} 
+}
+
+function addItem() {
+	var newItem = document.getElementById("newItem");
+	list.push(newItem.value);
+	newItem.focus();
+	newItem.value = "";
+	generateList();
+}
+
+function moveToTop(evt) {
+	if (evt === undefined) { //get caller element in IE8
+		evt = window.event;
+	}
+	var callerElement = evt.target || evt.srcElement;
+	var listItems = document.getElementsByTagName("li");
+	var parentItem = callerElement.parentNode;
+	for (var i = 0; i < list.length; i++) {
+		if (parentItem.innerHTML.search(list[i]) !== -1) {
+			var itemToMove = list.splice(i, 1);
+			list.unshift(itemToMove);
+		}
+	}
+	generateList();
+}
+
+function moveToBottom(evt) {
+	if (evt === undefined) { //get caller element in IE8
+		evt = window.event;
+	}
+	var callerElement = evt.target || evt.srcElement;
+	var listItems = document.getElementsByTagName("li");
+	var parentItem = callerElement.parentNode;
+	for (var i = 0; i < list.length; i++) {
+		if (parentItem.innerHTML.search(list[i]) !== -1) {
+			var itemToMove = list.splice(i, 1);
+			list.push(itemToMove);
+		}
+	}
+	generateList();
+}
+
+
+function createEventListeners() {
+	var addButton = document.getElementById("button");
+	if (addButton.addEventListener) {
+		addButton.addEventListener("click", addItem, false);
+	} else if (addButton.attachEvent) {
+		addButton.attachEvent("onclick", addItem);
+	}
+}
+if (window.addEventListener) {
+	window.addEventListener("load", createEventListeners, false);
+} else if (window.attachEvent) {
+	window.attachEvent("onload", createEventListeners);
+}
